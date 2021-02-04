0adb56e8ef6a8fd7811f0d8cacf47356815aaaa3
YunSeon2 <syys9293@gmail.com>
2021-02-04 13:36:02 +0900

    .

--- 다중상속문제.cpp
+++ 다중상속문제.cpp
@@ -19,6 +19,12 @@ public:
 		cout << "성별 입력: ";
 		cin >> m_strGender;
 	}
+	void PrintPerson()
+	{
+		cout << "나이: " << m_iAge << endl;
+		cout << "이름: " << m_strName << endl;
+		cout << "성별: " << m_strGender << endl;
+	}
 };
 class School
 {
@@ -36,24 +42,25 @@ public:
 		cout << "학생 번호 입력: ";
 		cin >> m_iNum;
 	}
+	void PrintSchool()
+	{
+		cout << m_iGrade << "학년 " << m_iGroup << "반 " << m_iNum << "번 학생" << endl;
+	}
 };
 class Student : public Person, public School
 {
 public:
 	void Print()
 	{
+		InputSchool();
+		InputPerson();
 		system("cls");
-		cout << m_iGrade << "학년 " << m_iGroup << "반 " << m_iNum << "번 학생" << endl;
-		cout << "나이: " << m_iAge << endl;
-		cout << "이름: " << m_strName << endl;
-		cout << "성별: " << m_strGender << endl;
-		system("pause");
+		PrintSchool();
+		PrintPerson();
 	}
 };
 void main()
 {
 	Student st;
-	st.InputSchool();
-	st.InputPerson();
 	st.Print();
 }
